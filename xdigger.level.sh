a042f893889602e35a3bc357ca14e8433ab47854
Tikkel <dr.tikkel@gmail.com>
2025-07-02 22:50:47 +0200

    arrays anpassungen in levelsxx.js

--- xdigger.level.sh
+++ xdigger.level.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 i=0;
+room_num=1;
 
 (
-echo -n "
-// SPDX-License-Identifier: GPL-3.0
-// jdigger/levels30.js - original Leveldatei von http://lanale.de/
+echo -n "// SPDX-License-Identifier: GPL-3.0
+// jdigger/levelsx1.js - original Leveldatei von http://lanale.de/
 // Copyright (C) 2017–2025  Marko Klingner
 
-room = new Array(
+room = [
 ";
+
 while [ $i -lt 4680 ]
 do
-	xxd -s$i -l156 -i -c10 -a xdigger.level; 
-	i=$[ $i+156 ]; 
+    xxd -s$i -l156 -i -c10 -a xdigger.level | \
+        sed "s|unsigned\ char\ xdigger\_level\[\]\ \=|\[ // Raum $room_num|g";
+    i=$[ $i+156 ];
+    room_num=$[ $room_num+1 ];
 done | \
-	sed 's|unsigned\ char\ xdigger\_level\[\]\ \=|new\ Array|g' | \
-	sed -z 's|\nunsigned\ int\ xdigger\_level\_len\ \=\ 156\;||g' | \
-	sed 's|\ {|\(|g' | \
-	sed -z 's|\n\}\;|\)\,|g' | \
-	sed '$aende' | \
-	sed -z 's|,\nende|\n);|g';
+    sed -z 's|\nunsigned\ int\ xdigger\_level\_len\ \=\ 156\;||g' | \
+    sed 's|\ {||g' | \
+    sed -z 's|\n\}\;|\]\,|g' | \
+    sed '$aende' | \
+    sed -z 's|,\nende|\n\];|g';
 ) > levelsx1.js
-
--- xdigger.level2.sh
+++ xdigger.level2.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 i=0;
+room_num=1;
 
 (
-echo -n "
-// SPDX-License-Identifier: GPL-3.0
-// jdigger/levels30.js - original Leveldatei von http://lanale.de/
+echo -n "// SPDX-License-Identifier: GPL-3.0
+// jdigger/levelsx2.js - original Leveldatei von http://lanale.de/
 // Copyright (C) 2017–2025  Marko Klingner
 
-room = new Array(
+room = [
 ";
+
 while [ $i -lt 4680 ]
 do
-	xxd -s$i -l156 -i -c10 -a xdigger.level2;
-	i=$[ $i+156 ];
+    xxd -s$i -l156 -i -c10 -a xdigger.level2 | \
+        sed "s|unsigned\ char\ xdigger\_level2\[\]\ \=|\[ // Raum $room_num|g";
+    i=$[ $i+156 ];
+    room_num=$[ $room_num+1 ];
 done | \
-	sed 's|unsigned\ char\ xdigger\_level2\[\]\ \=|new\ Array|g' | \
-	sed -z 's|\nunsigned\ int\ xdigger\_level2\_len\ \=\ 156\;||g' | \
-	sed 's|\ {|\(|g' | \
-	sed -z 's|\n\}\;|\)\,|g' | \
-	sed '$aende' | \
-	sed -z 's|,\nende|\n);|g';
+    sed -z 's|\nunsigned\ int\ xdigger\_level2\_len\ \=\ 156\;||g' | \
+    sed 's|\ {||g' | \
+    sed -z 's|\n\}\;|\]\,|g' | \
+    sed '$aende' | \
+    sed -z 's|,\nende|\n\];|g';
 ) > levelsx2.js
